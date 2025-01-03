c6b01f50ca59cc61d602db0e381739e4e0480e20
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-01-03 19:22:56 +0530

    code updated

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,36 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const bookingsRoutes = require('./api/bookings');
 
+const cors = require('cors');
+
+// Define allowed origins
+const allowedOrigins = [
+  'https://restaurant-table-booking-web-app-42kw.vercel.app', 
+ 
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);  
+    } else {
+      callback(new Error('Not allowed by CORS'), false);  
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+};
+
+// Apply the CORS middleware
+app.use(cors(corsOptions));
+
+
 // Initialize app and connect to DB
 const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
