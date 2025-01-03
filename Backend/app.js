8e54193acd7b7c1eac25bc780ce40d37ed2d93a3
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-01-03 19:03:58 +0530

    code updated

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const bookingsRoutes = require('./routes/bookings');
+const bookingsRoutes = require('./api/bookings');
 
 // Initialize app and connect to DB
 const app = express();
